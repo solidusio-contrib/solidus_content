c70ad63ba4647b86beb7674e657b24e4830ff438
Elia Schito <elia@schito.me>
2020-03-20 17:16:03 +0100

    Add a spec example for table_prefix

--- spec/solidus_content_spec.rb
+++ spec/solidus_content_spec.rb
@@ -3,6 +3,12 @@
 require 'spec_helper'
 
 RSpec.describe SolidusContent do
+  describe '.table_name_prefix' do
+    it 'returns the correct prefix' do
+      expect(described_class.table_name_prefix).to eq('solidus_content_')
+    end
+  end
+
   describe '.configure' do
     it 'yields the configuration' do
       expect { |b| described_class.configure(&b) }.to yield_with_args(described_class.config)
