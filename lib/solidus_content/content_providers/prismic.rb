847078710b83184bfacd0ddb6f26c31fef0243f8
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2020-04-03 12:50:58 +0200

    Add Prismic content provider

---
+++ lib/solidus_content/content_providers/prismic.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module SolidusContent::ContentProviders::Prismic
+  def self.call(input)
+    type_options = input.dig(:type_options)
+    entry_id = input.dig(:options, :id)
+
+    client = ::Prismic.api(
+      type_options[:api_entry_point],
+      type_options.dig(:api_token)
+    )
+
+    entry = client.getByID(entry_id)
+
+    input.merge(
+      data: entry.fields,
+      provider_client: client,
+      provider_entry: entry,
+    )
+  end
+
+  SolidusContent.config.content_providers[:prismic] = self
+end
