e4ab57c254b02c3d9f49d3574af72aa4002202c0
Elia Schito <elia@schito.me>
2020-04-17 18:29:54 +0200

    Move the configuration docs to the generated initializer

--- lib/generators/solidus_content/install/templates/initializer.rb
+++ lib/generators/solidus_content/install/templates/initializer.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 SolidusContent.configure do |config|
+  # You can control the list of content providers that will be available to the 
+  # backend.
+  # 
+  # config.content_providers.delete :prismic
+
+  # Define a custom content provider.
+  # 
+  # config.register_content_provider :data_dir, ->(input) {
+  #   data = YAML.load(Rails.root.join('data', input[:slug] + '.yml').read)
+  #   input.merge(data: data)
+  # }
+
+  # Set to `true` to register your own route, instead of using the default
+  # that starts with `/c/`.
+  # 
+  # config.skip_default_route = true
 end
