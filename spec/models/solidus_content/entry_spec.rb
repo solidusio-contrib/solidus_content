bde87c822986ecc07a0aebccb480ef9897e83e07
Peter Berkenbosch <info@peterberkenbosch.nl>
2020-05-19 12:47:38 +0200

    Use the same quotes and proper method descriptions
    Signed-off-by: Peter Berkenbosch <info@peterberkenbosch.nl>

--- spec/models/solidus_content/entry_spec.rb
+++ spec/models/solidus_content/entry_spec.rb
@@ -1,22 +1,28 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require "spec_helper"
 
 RSpec.describe SolidusContent::Entry do
   let(:entry) { build(:entry) }
 
-  it 'has a valid factory' do
+  it "has a valid factory" do
     expect(entry).to be_valid
   end
 
-  describe "#data_for" do
+  describe ".data_for" do
     let(:post) { create(:entry_type, name: :post, provider_name: :raw) }
-    let!(:post_one) { create(:entry, slug: 'one', options: {title: "first post"}, entry_type: post)}
+    let!(:post_one) do
+      create(:entry,
+        slug: "one",
+        options: { title: "first post" },
+        entry_type: post
+      )
+    end
 
     context "with using correct type and slug" do
       it "returns data" do
         expect(
-          SolidusContent::Entry.data_for(:post, 'one')
+          SolidusContent::Entry.data_for(:post, "one")
         ).to eq({title: "first post"})
       end
     end
@@ -24,7 +30,7 @@ RSpec.describe SolidusContent::Entry do
     context "with using invalid type" do
       it "will raise an ActiveRecord::RecordNotFound exception" do
         expect{
-          SolidusContent::Entry.data_for(:foo, 'one')
+          SolidusContent::Entry.data_for(:foo, "one")
         }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
@@ -32,13 +38,13 @@ RSpec.describe SolidusContent::Entry do
     context "with using an invalid slug" do
       it "will raise an ActiveRecord::RecordNotFound exception" do
         expect{
-          SolidusContent::Entry.data_for(:post, 'foo')
+          SolidusContent::Entry.data_for(:post, "foo")
         }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
 
-  specify '#data' do
+  specify "#data" do
     expect(entry.entry_type)
       .to receive(:content_for).with(entry).and_return({data: :foo})
 
@@ -46,7 +52,7 @@ RSpec.describe SolidusContent::Entry do
   end
 
   specify "#content" do
-    content = double('content')
+    content = double("content")
 
     expect(entry.entry_type)
       .to receive(:content_for).with(entry).and_return(content)
