7a39c14c7993e40adb106d1d6b4df21f103884e2
Elia Schito <elia@schito.me>
2020-05-29 13:05:52 +0200

    Restore default resource-controller url helpers
    Co-authored-by: Andrea Vassallo <andrea.vassallo.94@gmail.com>

--- app/controllers/spree/admin/entry_types_controller.rb
+++ app/controllers/spree/admin/entry_types_controller.rb
@@ -25,8 +25,20 @@ module Spree
         params.require(:solidus_content_entry_type).permit(:name, :provider_name)
       end
 
-      def location_after_save
-        admin_entry_types_path
+      def new_object_url(options = {})
+        new_admin_entry_type_path(options)
+      end
+
+      def edit_object_url(object, options = {})
+        edit_admin_entry_type_path(object, options)
+      end
+
+      def object_url(object = nil, options = {})
+        admin_entry_type_path((object || @object), options)
+      end
+
+      def collection_url(options = {})
+        admin_entry_types_path(options)
       end
     end
   end
