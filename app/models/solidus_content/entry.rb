1725947966c056cce289c81aaf421cf96915d88c
Elia Schito <elia@schito.me>
2020-06-12 17:27:06 +0200

    Sort out methods and scopes in Entry
    Just moves around code.

--- app/models/solidus_content/entry.rb
+++ app/models/solidus_content/entry.rb
@@ -2,19 +2,19 @@
 
 class SolidusContent::Entry < ActiveRecord::Base
   belongs_to :entry_type
+
   after_initialize { self.options ||= {} }
 
   scope :by_slug, ->(slug) { find_by!(slug: slug) }
-
-  def self.data_for(type, slug)
-    by_type(type).by_slug(slug).data
-  end
-
-  def self.by_type(type)
+  scope :by_type, ->(type) {
     unless type.is_a? SolidusContent::EntryType
       type = SolidusContent::EntryType.by_name(type)
     end
     where(entry_type: type)
+  }
+
+  def self.data_for(type, slug)
+    by_type(type).by_slug(slug).data
   end
 
   def data
